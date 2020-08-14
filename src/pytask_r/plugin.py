fe855918efb4475a3045d5dfd70ea0d0c8941abe
Tobias Raabe <raabe@posteo.de>
2020-08-14 14:20:43 +0200

    Fix.

--- src/pytask_r/plugin.py
+++ src/pytask_r/plugin.py
@@ -1,11 +1,11 @@
-import pytask
+from _pytask.config import hookimpl
 from pytask_r import collect
 from pytask_r import config
 from pytask_r import execute
 from pytask_r import parametrize
 
 
-@pytask.hookimpl
+@hookimpl
 def pytask_add_hooks(pm):
     pm.register(collect)
     pm.register(config)
