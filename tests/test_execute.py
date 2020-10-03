4f274ee8be089937667bc40f831e9cb607073988
Tobias Raabe <raabe@posteo.de>
2020-10-03 13:28:17 +0200

    Some fixes.

--- tests/test_execute.py
+++ tests/test_execute.py
@@ -62,42 +62,6 @@ def test_run_r_script(tmp_path):
     assert tmp_path.joinpath("out.txt").exists()
 
 
-@needs_rscript
-@pytest.mark.end_to_end
-def test_parametrized_execution_of_r_script(tmp_path):
-    task_source = """
-    import pytask
-
-    @pytask.mark.r
-    @pytask.mark.parametrize("depends_on, produces", [
-        ("script_1.r", "0.txt"),
-        ("script_2.r", "1.txt"),
-    ])
-    def task_run_r_script():
-        pass
-    """
-    tmp_path.joinpath("task_dummy.py").write_text(textwrap.dedent(task_source))
-
-    for name, content, out in [
-        ("script_1.r", "Cities breaking down on a camel's back", "0.txt"),
-        ("script_2.r", "They just have to go 'cause they don't know whack", "1.txt"),
-    ]:
-        r_script = f"""
-        file_descr <- file("{out}")
-        writeLines(c("{content}"), file_descr)
-        close(file_descr)
-        """
-        tmp_path.joinpath(name).write_text(textwrap.dedent(r_script))
-
-    os.chdir(tmp_path)
-    session = main({"paths": tmp_path})
-
-    assert session.exit_code == 0
-    assert tmp_path.joinpath("0.txt").exists()
-    assert tmp_path.joinpath("1.txt").exists()
-
-
-@needs_rscript
 @pytest.mark.end_to_end
 def test_raise_error_if_rscript_is_not_found(tmp_path, monkeypatch):
     task_source = """
