d19ce47611bdee5c20dc1755a18c4dee16ade82d
Tobias Raabe <raabe@posteo.de>
2022-04-06 00:40:02 +0200

    Make pytask-r work with toml, change readme.

--- src/pytask_r/config.py
+++ src/pytask_r/config.py
@@ -10,5 +10,18 @@ def pytask_parse_config(config, config_from_file):
     config["markers"]["r"] = "Tasks which are executed with Rscript."
     config["r_serializer"] = config_from_file.get("r_serializer", "json")
     config["r_suffix"] = config_from_file.get("r_suffix", "")
-    options = config_from_file.get("r_options")
-    config["r_options"] = options.split(" ") if isinstance(options, str) else []
+    config["r_options"] = _parse_value_or_whitespace_option(
+        config_from_file.get("r_options")
+    )
+
+
+def _parse_value_or_whitespace_option(value: str | None) -> None | str | list[str]:
+    """Parse option which can hold a single value or values separated by new lines."""
+    if value in ["none", "None", None, ""]:
+        return None
+    elif isinstance(value, list):
+        return list(map(str, value))
+    elif isinstance(value, str):
+        return [v.strip() for v in value.split(" ") if v.strip()]
+    else:
+        raise ValueError(f"Input {value!r} is neither a 'str' nor 'None'.")
