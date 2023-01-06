7ba227540b0651b3c237d33d222d799466cc703a
Tobias Raabe <raabe@posteo.de>
2023-01-06 14:11:02 +0100

    Fix hooks.

--- src/pytask_r/config.py
+++ src/pytask_r/config.py
@@ -16,9 +16,7 @@ def pytask_parse_config(config):
             f"{list(SERIALIZERS)}."
         )
     config["r_suffix"] = config.get("r_suffix", "")
-    config["r_options"] = _parse_value_or_whitespace_option(
-        config.get("r_options")
-    )
+    config["r_options"] = _parse_value_or_whitespace_option(config.get("r_options"))
 
 
 def _parse_value_or_whitespace_option(value: str | None) -> None | str | list[str]:
